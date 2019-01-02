589af49954a551ebf0ef248f2e09d417d201593b
JesseTzh <867444083@qq.com>
2019-01-02 16:22:11 +0800

    数据插入

---
+++ application/insert_data/model/Customer.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 86744
+ * Date: 2019/1/1
+ * Time: 16:38
+ */
+
+namespace app\insert_data\model;
+
+use think\Model;
+
+class Customer extends Model
+{
+}
