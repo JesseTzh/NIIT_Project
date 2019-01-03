55f94170ca70ecc3925b2f2392ad1b5e3c1338fe
tony <life0531@foxmail.com>
2019-01-03 15:28:22 +0800

    分离角色模块，session增加部门ID存储

---
+++ application/admin/controller/Admin.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: life0
+ * Date: 2019/1/3
+ * Time: 13:27
+ */
+
+namespace app\admin\Controller;
+
+use think\Controller;
+use app\index\controller\Customer;
+use app\employee\controller\Employee;
+
+class Admin extends Controller{
+    public function admin_customer(){
+//        return $this->fetch('admin');
+//        dump(config());
+        $customer = new Customer;
+        $customerList = $customer->lists_val();
+        $this->assign('customerList', $customerList);
+        return $this->fetch('admin_customer');
+    }
+
+    public function admin_employee(){
+        //员工管理
+        $employee = new Employee;
+        $employeerList = $employee->lists_val();
+        $this->assign('employeeList', $employeerList);
+        return $this->fetch('admin_employee');
+    }
+}
