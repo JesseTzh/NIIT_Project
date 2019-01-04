74712a2ef024fb4502d21cfd07d229e4b155ee87
zhpjy <zhpjy@qq.com>
2019-01-04 17:44:34 +0800

    完成导入excel功能

--- extend/Utils/OfficeUtils.php
+++ extend/Utils/OfficeUtils.php
@@ -1,6 +1,5 @@
 <?php
 namespace Utils;
-use PhpOffice\PhpSpreadsheet\Spreadsheet;
 
 class OfficeUtils{
 
@@ -20,28 +19,32 @@ class OfficeUtils{
 
     public static function read($file_path){
         $file_format = substr($file_path, strrpos($file_path, '.')+1);
-        if($file_format == 'xlsx'){
-            $xlsx = new \PhpOffice\PhpSpreadsheet\Reader\Xlsx();
-            $can_read = $xlsx->canRead($file_path);
-            if($can_read){
-                $content = $xlsx->load($file_path)->getActiveSheet()->toArray(); 
-                $content = OfficeUtils::arrayCover($content);
-                return $content;
+        try{
+            if($file_format == 'xlsx'){
+                $xlsx = new \PhpOffice\PhpSpreadsheet\Reader\Xlsx();
+                $can_read = $xlsx->canRead($file_path);
+                if($can_read){
+                    $content = $xlsx->load($file_path)->getActiveSheet()->toArray(); 
+                    $content = OfficeUtils::arrayCover($content);
+                    return $content;
+                }else{
+                    return false;
+                }
+            }elseif ($file_format == 'xls'){
+                $xls = new \PhpOffice\PhpSpreadsheet\Reader\Xls();
+                $can_read = $xls->canRead($file_path);
+                if($can_read){
+                    $content = $xls->load($file_path)->getActiveSheet()->toArray();
+                    return $content;
+                }else{
+                    return false;
+                }
             }else{
                 return false;
-            }
-        }elseif ($file_format == 'xls'){
-            $xls = new \PhpOffice\PhpSpreadsheet\Reader\Xls();
-            $can_read = $xls->canRead($file_path);
-            if($can_read){
-                $content = $xls->load($file_path)->getActiveSheet()->toArray();
-                return $content;
-            }else{
-                return false;
-            }
-        }else{
+            } 
+        }catch(Exception $e){
             return false;
-        } 
+        }
     }
 
     public static function wirte(){
